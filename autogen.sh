9cf9e92a3e554728fc58cbeb64d6ae10accd30d9
Michael Terry <michael.terry@canonical.com>
2010-01-27 16:34:42 -0500

    modernize autotools support a bit

--- autogen.sh
+++ autogen.sh
@@ -5,10 +5,6 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="deja-dup"
-REQUIRED_AUTOCONF_VERSION=2.53
-REQUIRED_AUTOMAKE_VERSION=1.9
-REQUIRED_GETTEXT_VERSION=0.17
-REQUIRED_INTLTOOL_VERSION=0.37
 
 (test -f $srcdir/configure.ac \
   && test -d $srcdir/deja-dup) || {
